ecb113de02376fce17513e35923a0cf9aaa1b2ae
KwonBoWon <bogod1207@naver.com>
2021-02-15 14:13:48 +0000

    use string fungtions

--- main.py
+++ main.py
@@ -3,13 +3,5 @@ while True:
   N=input()
   if N=="0":
     break
-  N=list(N)
-  NN=N
-  N="".join(N)
-  NN.reverse()
-  NN="".join(NN)
-  if N==NN:
-    print("yes")
-  else:
-    print("no")
+  print("yes" if N==N[::-1] else "no")
   #print(N)
